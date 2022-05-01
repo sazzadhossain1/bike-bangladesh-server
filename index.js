ecda162e16bc619913d1bb1b26cfa8d1b7fcecd3
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-02 00:20:46 +0600

    add lintening

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send('Running my node crud server');
+app.get("/", (req, res) => {
+  res.send("Running my node crud server");
 });
-app.listen(port, () =>{
-    console.log('CRUD Server is running');
-})
+
+app.listen(port, () => {
+  console.log("port is listening", port);
+});
