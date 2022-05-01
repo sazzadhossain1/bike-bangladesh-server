63eb2b50868343435003d1d0759c6c5b26347524
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-02 01:07:37 +0600

    added async funcrion

--- index.js
+++ index.js
@@ -3,9 +3,34 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bplvs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("bikeBangladesh").collection("bike");
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const product = await cursor.toArray();
+      res.send(product);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.log);
 
 app.get("/", (req, res) => {
   res.send("Running my node crud server");
